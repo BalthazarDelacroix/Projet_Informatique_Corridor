ad650a3365781f90aeebc92d00f8dc5ababef251
Austinwangg <austin.wang@edu.ece.fr>
2024-10-18 12:44:23 +0200

    iii

---
+++ cmake-build-debug/CMakeFiles/projet_info_je_vais_baiser_austin_.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet_info_je_vais_baiser_austin_.dir/main.c.obj"
+  "CMakeFiles/projet_info_je_vais_baiser_austin_.dir/main.c.obj.d"
+  "libprojet_info_je_vais_baiser_austin_.dll.a"
+  "projet_info_je_vais_baiser_austin_.exe"
+  "projet_info_je_vais_baiser_austin_.exe.manifest"
+  "projet_info_je_vais_baiser_austin_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet_info_je_vais_baiser_austin_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
