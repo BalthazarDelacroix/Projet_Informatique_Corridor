b497dfa460397a5fdc5d547c453418f4d084fdb5
BalthazarDelacroix <balthazar.delacroix@edu.ece.fr>
2024-10-23 17:19:54 +0200

    ij

--- Code Maxence.h
+++ Code Maxence.h
@@ -9,4 +9,5 @@
 void initPlateau(char plateau[TAILLE][TAILLE]);
 void afficherPlateau(char plateau[TAILLE][TAILLE]);
 void placerMur(char plateau[TAILLE][TAILLE], int x, int y, char typeMur);
+
 #endif
